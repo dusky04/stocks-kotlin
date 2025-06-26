f6aecd2efa4d39bb9d424a89adb7edae27643a09
Vaibhav Rastogi <rastogi04vaibhav@gmail.com>
2025-06-26 17:59:01 +0530

    Added a searchbar not functional yet

--- app/src/main/java/com/example/stocks/MainActivity.kt
+++ app/src/main/java/com/example/stocks/MainActivity.kt
@@ -1,22 +1,35 @@
 package com.example.stocks
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -61,6 +74,7 @@ fun StocksApp(stocksViewModel: StocksViewModel) {
     StocksTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
             Scaffold(
+                topBar = { Search() },
                 bottomBar = { BottomNavBar(navController) }) { innerPadding ->
                 NavHost(
                     navController = navController,
@@ -81,5 +95,32 @@ fun StocksApp(stocksViewModel: StocksViewModel) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun Search() {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    val textFieldState = rememberTextFieldState()
+    SearchBar(
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = textFieldState.text.toString(),
+                onQueryChange = { newQuery ->
+                    textFieldState.edit {
+                        replace(0, length, newQuery)
+                    }
+                },
+                onSearch = {
+                    Log.i("SEARCH TRIGGERD", expanded.toString())
+                },
+                expanded = expanded,
+                onExpandedChange = { it -> { expanded = it } },
+                placeholder = { Text("Search") },
+                leadingIcon = { Icon(Icons.Default.Search, null) }
+            )
+        }, expanded = expanded, onExpandedChange = { it -> { expanded = it } }
+    ) { }
+}
 
 
--- app/src/main/java/com/example/stocks/screens/HomeScreen.kt
+++ app/src/main/java/com/example/stocks/screens/HomeScreen.kt
@@ -1,15 +1,32 @@
 package com.example.stocks.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.stocks.StocksViewModel
+import com.example.stocks.data.TopGainer
 
 @Composable
 fun HomeScreen(viewModel: StocksViewModel, modifier: Modifier = Modifier) {
+
+    LaunchedEffect(true) {
+        // `true` will make this run only once
+        viewModel.getCompanyOverviewData("AAPL")
+    }
+
+
     Column {
+        CardComponent(stock)
+
         Text("Home Screen", modifier = modifier)
         Button(
             onClick = { viewModel.searchTicker("AAPL") }
@@ -21,4 +38,18 @@ fun HomeScreen(viewModel: StocksViewModel, modifier: Modifier = Modifier) {
             onClick = { viewModel.getCompanyOverviewData("AAPL") }
         ) { Text("Click Here to get company data") }
     }
+}
+
+val stock = TopGainer("3.71", "82.8125%", "8.19", "ULY", "23887434")
+
+
+@Composable
+fun CardComponent(cardInfo: TopGainer) {
+    ElevatedCard(
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFCAF1DE),
+        ),
+        modifier = Modifier
+            .size(width = 150.dp, height = 250.dp)
+    ) { Text(cardInfo.ticker) }
 }
