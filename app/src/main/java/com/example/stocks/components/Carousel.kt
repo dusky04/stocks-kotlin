5a438d89493a7dc501f8b16d738b26782e905748
Vaibhav Rastogi <rastogi04vaibhav@gmail.com>
2025-06-27 17:15:18 +0530

    Updated the UI

--- app/src/main/java/com/example/stocks/components/Carousel.kt
+++ app/src/main/java/com/example/stocks/components/Carousel.kt
@@ -1,27 +1,24 @@
 package com.example.stocks.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.carousel.HorizontalUncontainedCarousel
-import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.stocks.data.TopGainerLoser
 import com.example.stocks.ui.theme.sansFontFamily
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Carousel(sectionTitle: String, itemsList: List<TopGainerLoser>, isGainer: Boolean) {
-    Column(modifier = Modifier.padding(12.dp)) {
+    Column(modifier = Modifier.padding(12.dp), verticalArrangement = Arrangement.spacedBy(10.dp)) {
         Text(
             sectionTitle,
             fontFamily = sansFontFamily,
@@ -29,17 +26,13 @@ fun Carousel(sectionTitle: String, itemsList: List<TopGainerLoser>, isGainer: Bo
             style = MaterialTheme.typography.titleLarge
         )
         if (itemsList.isNotEmpty()) {
-            HorizontalUncontainedCarousel(
-                itemWidth = 170.dp,
-                itemSpacing = 4.dp,
-                state = rememberCarouselState { itemsList.count() },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .clipToBounds(),
-            ) { idx ->
-                val item = itemsList[idx]
-                StockCard(item, isGainer)
+            val visibleItems = itemsList.take(6)
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+            ) {
+                items(itemsList) { item ->
+                    StockCard(item, isGainer)
+                }
             }
         } else {
             CircularProgressIndicator()
--- app/src/main/java/com/example/stocks/components/StockCard.kt
+++ app/src/main/java/com/example/stocks/components/StockCard.kt
@@ -5,100 +5,114 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.stocks.LocalNavController
 import com.example.stocks.assets.TrendingDownIcon
 import com.example.stocks.assets.TrendingUpIcon
 import com.example.stocks.data.TopGainerLoser
+import com.example.stocks.ui.theme.sansFontFamily
 
 
 @Composable
 fun StockCard(cardInfo: TopGainerLoser, isGainer: Boolean) {
     val navController = LocalNavController.current
-    ElevatedCard(
+    Card(
         colors = CardDefaults.cardColors(containerColor = Color.White),
         modifier = Modifier
-            .padding(horizontal = 0.dp, vertical = 12.dp)
-            .width(170.dp)
-            .height(130.dp),
-        shape = RoundedCornerShape(16.dp),
-        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp),
+            .fillMaxWidth()
+            .padding(4.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         onClick = {
             navController.navigate("overview/${cardInfo.ticker}/${cardInfo.price}/${cardInfo.changeAmount}/${cardInfo.changePercentage}")
         }
     ) {
-        Column(
+        Row(
             modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceBetween
+                .fillMaxWidth()
+                .padding(top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
+
             Row(
-                modifier = Modifier
-                    .background(Color.LightGray)
-                    .padding(12.dp),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
             ) {
-                Text(
-                    cardInfo.ticker,
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.weight(1f)
-                )
                 Box(
                     modifier = Modifier
-                        .size(32.dp)
-                        .background(color = Color(0xFFE0D7C8), shape = CircleShape),
+                        .size(36.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(if (isGainer) Color(0xffE6F4EA) else Color(0xFFFCE8E6)),
                     contentAlignment = Alignment.Center
                 ) {
                     Icon(
-                        imageVector = Icons.Default.Call,
+                        imageVector = if (isGainer) TrendingUpIcon else TrendingDownIcon,
                         contentDescription = null,
-                        tint = Color.White,
-                        modifier = Modifier.size(18.dp)
+                        tint = if (isGainer) Color(0xFF4CAF50) else Color(0xFFF44336),
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+                }
+                Column {
+                    Text(
+                        text = cardInfo.ticker,
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 16.sp
+                        ),
+                        fontFamily = sansFontFamily,
+                        color = Color.Black
+                    )
+                    Text(
+                        text = "%.2f".format(cardInfo.price.toFloat()),
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            fontSize = 14.sp
+                        ),
+                        fontFamily = sansFontFamily,
+                        color = Color.Black
                     )
                 }
             }
+
+            // Right side - Price and percentage change
             Column(
-                modifier = Modifier.padding(12.dp),
-                horizontalAlignment = Alignment.Start
+                horizontalAlignment = Alignment.End
             ) {
                 Text(
-                    text = "$${cardInfo.price}",
-                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                    text = "+%.2f".format(cardInfo.changePercentage.dropLast(1).toFloat()) + "%",
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 14.sp
+                    ),
+                    fontFamily = sansFontFamily,
+                    color = if (isGainer) Color(0xFF4CAF50) else Color(0xFFF44336)
                 )
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Icon(
-                        imageVector = if (isGainer) TrendingUpIcon else TrendingDownIcon,
-                        contentDescription = null,
-                        tint = if (isGainer) Color(0xFF2ECC71) else Color(0xFFB71C1C),
-                        modifier = Modifier.size(16.dp)
-                    )
-                    Text(
-                        text = cardInfo.changePercentage,
-                        color = if (isGainer) Color(0xFF2ECC71) else Color(0xFFB71C1C),
-                        style = MaterialTheme.typography.bodySmall,
-                        fontWeight = FontWeight.Medium
-                    )
-                }
+                Text(
+                    text = "+" + cardInfo.changeAmount,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontSize = 14.sp
+                    ),
+                    fontFamily = sansFontFamily,
+                    color = if (isGainer) Color(0xFF4CAF50) else Color(0xFFF44336)
+                )
+
             }
         }
     }
